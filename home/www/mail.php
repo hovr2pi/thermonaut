d78956f55f60eb7833bd148a65e8554e54cec493
Christopher Simmons <csim@hovr2pi.org>
2013-11-25 08:18:08 -0600

    adding a email button and php script to send it

---
+++ home/www/mail.php
@@ -0,0 +1,14 @@
+unction sendemail()
+{
+    var url = '/mail.php';
+
+    new Ajax.Request(url,{
+            onComplete:function(transport)
+            {
+                var feedback = transport.responseText.evalJSON();
+                if(feedback.result==0)
+                    alert('There was a problem sending the email, please try again.');
+            }
+        });
+
+}
